97fbf6cd270919c41d65140dc48e80d486374930
Olia <oliaopanasiukk@gmail.com>
2024-04-29 23:30:49 +0300

    initial commit

---
+++ MessageClientApp/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MessageClientApp
+{
+
+    public partial class MainWindow : Window
+    {
+        IPEndPoint serverPoint;
+        UdpClient client;
+        ObservableCollection<MessageInfo> messages;
+        private bool isJoined = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string serverAddress = ConfigurationManager.AppSettings["serverAddress"]!;
+            short serverPort = short.Parse(ConfigurationManager.AppSettings["serverPort"]!);
+            serverPoint = new IPEndPoint(IPAddress.Parse(serverAddress), serverPort);
+            client = new UdpClient();
+            messages = new ObservableCollection<MessageInfo>();
+            this.DataContext = messages;
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = msgTextBox.Text;
+            if (!isJoined)
+            {
+                MessageBox.Show("You must join the chat before sending a message");
+                return;
+            }
+            if (string.IsNullOrEmpty(msg))
+            {
+                MessageBox.Show("Here is nothing to send");
+                return;
+            }
+            else
+            {
+                msgTextBox.Text = "";
+                MessageInfo clientMsg = new MessageInfo(msg, usernameBox.Text);
+                string message = JsonSerializer.Serialize<MessageInfo>(clientMsg);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                SendMessage(data);
+            }
+        }
+        private async void SendMessage(byte[] data)
+        {
+            await client.SendAsync(data, data.Length, serverPoint);
+        }
+        private void msgTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendButton_Click(sender, e);
+            }
+        }
+
+        private void JoinButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(usernameBox.Text) 
+                || string.IsNullOrWhiteSpace(usernameBox.Text))
+                MessageBox.Show("Enter your name before joining the chat");
+
+            else
+            {
+                string message = "$<Join>";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                string name = usernameBox.Text;
+                textLabel.Content = name;
+                usernameBox.Visibility = Visibility.Hidden;
+                textLabel.Visibility = Visibility.Visible;
+                MessageBox.Show("Welcome to our chat!!");
+                SendMessage(data);
+                Listener();
+                isJoined = true;
+            }
+        }
+
+        private async void Listener()
+        {
+            while (true)
+            {
+                var res = await client.ReceiveAsync();
+                string message = Encoding.Unicode.GetString(res.Buffer);
+                MessageInfo clientMsg = JsonSerializer.Deserialize<MessageInfo>(message)!;
+                messages.Add(clientMsg);
+            }
+        }
+        private void LeaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "$<Leave>";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            SendMessage(data);
+            usernameBox.Clear();
+            msgTextBox.Clear();
+            messages.Clear();
+            textLabel.Content = "";
+
+            usernameBox.Visibility = Visibility.Visible;
+            textLabel.Visibility = Visibility.Hidden;
+        }
+    }
+    public class MessageInfo
+    {
+        public string Message { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public MessageInfo(string message, string name)
+        {
+            Message = message;
+            Name = name;
+            Time = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"{Name} -- {Message,-20} {Time.ToShortTimeString()}";
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+public class ChatServer
+{
+    const short port = 4040;
+    const string JOIN = "$<Join>";
+    const string LEAVE = "$<Leave>";
+    UdpClient server;
+    IPEndPoint clientEndPoint = null;
+    List<IPEndPoint> members;
+
+    public ChatServer()
+    {
+        server = new UdpClient(port);
+        members = new List<IPEndPoint>();
+    }
+    public void Start()
+    {
+        while (true)
+        {
+            byte[] data = server.Receive(ref clientEndPoint);
+            string message = Encoding.Unicode.GetString(data);
+
+            switch (message)
+            {
+                case JOIN:
+                    AddMember(clientEndPoint);
+                    break;
+                case LEAVE:
+                    RemoveMember(clientEndPoint);
+                    break;
+                default:
+                    Console.WriteLine($"Got message {message,-20} from : {clientEndPoint} " +
+                        $"at {DateTime.Now.ToShortTimeString()}");
+                    SendToAll(data);
+                    break;
+            }
+        }
+    }
+    private void AddMember(IPEndPoint clientEndPoint)
+    {
+        if (!members.Contains(clientEndPoint))
+        {
+            members.Add(clientEndPoint);
+            Console.WriteLine("Member was added");
+        }
+    }
+    private void RemoveMember(IPEndPoint clientEndPoint)
+    {
+        if (members.Contains(clientEndPoint))
+        {
+            members.Remove(clientEndPoint);
+            Console.WriteLine("Member deleted");
+        }
+
+    }
+    private async void SendToAll(byte[] data)
+    {
+        foreach (var member in members)
+        {
+            await server.SendAsync(data, data.Length, member);
+        }
+    }
+}
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        ChatServer server = new ChatServer();
+        server.Start();
+    }
+}
